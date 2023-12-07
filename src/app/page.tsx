e8abcc8cea31372d16b044e121739b5d6366d35c
smurf <smurf@kakao.com>
2023-12-08 04:41:13 +0900

    wip auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,22 @@
 import Link from 'next/link';
+import styles from '@/app/page.module.css';
 //vanila extract ? 하기 ?
 export default function Home() {
   return (
-    <main>
-      <h1>타이틀</h1>
-      <h2>로그인하세요</h2>
-      <Link href='/auth/login'>로그인</Link>
-      <h2>or</h2>
-      <Link href='/auth/login'>계정 만들기</Link>
-      {/* 페이지 라우팅 */}
-      {/* redirect(); */}
+    <main className={styles.container}>
+      <div className={styles.center}>
+        <h1>타이틀</h1>
+        <h2>로그인하세요</h2>
+        <Link href='/auth/login' className={`${styles.btn} ${styles.login}`}>
+          로그인
+        </Link>
+        <h3>or</h3>
+        <Link href='/auth/login' className={`${styles.btn} ${styles.signup}`}>
+          계정 만들기
+        </Link>
+        {/* 페이지 라우팅 */}
+        {/* redirect(); */}
+      </div>
     </main>
   );
 }
