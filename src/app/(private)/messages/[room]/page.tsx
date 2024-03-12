df29a6f1fde7ee903402cbd099a35dd2d60221fc
smurf <smurf@kakao.com>
2024-03-13 01:30:33 +0900

    feat : messages done

--- src/app/(private)/messages/[room]/page.tsx
+++ src/app/(private)/messages/[room]/page.tsx
@@ -1,6 +1,13 @@
 import { faker } from '@faker-js/faker';
 import styles from './chatRoom.module.css';
 import Link from 'next/link';
+import BackButton from '../../_component/BackButton';
+import cx from 'classnames';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+dayjs.locale('ko');
+dayjs.extend(relativeTime);
 export default function ChatRoom() {
   const user = {
     id: 'userId',
@@ -26,35 +33,46 @@ export default function ChatRoom() {
   return (
     <main className={styles.main}>
       <div className={styles.header}>
-        <Link
-          href={user.nickname}
-          className={styles.userInfo}>
-          <img
-            src={user.image}
-            alt={user.id}
-          />
-          <div>@{user.id}</div>
-        </Link>
-        <div className={styles.list}>
-          {messages.map(m => {
-            if (m.id === 'me') {
-              return (
-                <div
-                  key={m.messageId}
-                  className={styles.myMessage}>
-                  {m.content}
-                </div>
-              );
-            }
+        <BackButton />
+        <div>
+          <h2>{user.nickname}</h2>
+        </div>
+      </div>
+
+      <Link
+        href={user.nickname}
+        className={styles.userInfo}>
+        <img
+          src={user.image}
+          alt={user.id}
+        />
+        <div>@{user.id}</div>
+      </Link>
+      <div className={styles.list}>
+        {messages.map(m => {
+          if (m.id === 'me') {
             return (
               <div
                 key={m.messageId}
-                className={styles.yourMessage}>
-                {m.content}
+                className={cx(styles.message, styles.myMessage)}>
+                <div className={styles.content}>{m.content}</div>
+                <div className={styles.date}>
+                  {dayjs(m.createdAt).format('YYYY년 MM월 DD일 A HH시mm분')}
+                </div>
               </div>
             );
-          })}
-        </div>
+          }
+          return (
+            <div
+              key={m.messageId}
+              className={cx(styles.message, styles.yourMessage)}>
+              <div className={styles.content}>{m.content}</div>
+              <div className={styles.date}>
+                {dayjs(m.createdAt).format('YYYY년 MM월 DD일 A HH시mm분')}
+              </div>
+            </div>
+          );
+        })}
       </div>
     </main>
   );
